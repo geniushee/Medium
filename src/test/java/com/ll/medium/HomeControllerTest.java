ca7e2885ad018f022a4d86e08eaf4dc95403c569
geniushee <h1116jeon@gmail.com>
2023-12-05 14:40:57 +0900

    init

---
+++ src/test/java/com/ll/medium/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package com.ll.medium;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class HomeControllerTest {
+
+    @Test
+    @DisplayName("t1")
+    public void t1(){
+
+    }
+}
