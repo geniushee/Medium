4542f8bd9cce4924b7bd64e7853d205a1e869041
geniushee <h1116jeon@gmail.com>
2023-12-14 00:34:03 +0900

    modify method & some function

--- src/main/java/com/ll/medium/domain/aticle/article/controller/ArticleController.java
+++ src/main/java/com/ll/medium/domain/aticle/article/controller/ArticleController.java
@@ -35,7 +35,6 @@ public class ArticleController {
 
     // TODO 내 글 목록 조회: 내 글 리스트 조회 get - '/post/myList'
 
-    // TODO 글 상세내용 조회: ?번 글 상세보기 get - '/post/1''
     @GetMapping("/{id}")
     public String showArticleDetails(@PathVariable("id") long id,
                                      Model model) {
@@ -59,25 +58,24 @@ public class ArticleController {
         return rq.redirect("domain/article/article/list", "글 작성이 성공적으로 완료되었습니다.");
     }
 
-    // TODO 글 수정 : 글 수정 폼 get - '/post/1/modify'
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}/modify")
     public String showModifyForm(@PathVariable("id") long id,
                                  Model model){
-        // TODO 유저 확인 필요
         ArticleDto dto = articleService.findById(id);
+        if (dto.getAuthor() != rq.getMember()){
+            return rq.redirect("/post/list", "작성자가 아닙니다.");
+        }
         model.addAttribute("article", dto);
         return "domain/article/article/modifyForm";
     }
 
-    // TODO 글 수정 : 글 수정 처리 post - '/post/1/modify'
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/{id}/modify")
     public String modifyArticle(@PathVariable("id") long id,
                                 @Valid ArticleForm articleForm,
                                 BindingResult bindingResult,
                                 Model model){
-        // TODO 유저 확인 필요
         if (bindingResult.hasErrors()){
             throw new RuntimeException("잘못된 입력입니다.");
         }
