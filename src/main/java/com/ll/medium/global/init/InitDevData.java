9467c9065b625317ef666b1178811a021d8b6ccb
geniushee <h1116jeon@gmail.com>
2023-12-13 15:22:02 +0900

    create test datas in initdata

--- src/main/java/com/ll/medium/global/init/InitDevData.java
+++ src/main/java/com/ll/medium/global/init/InitDevData.java
@@ -1,6 +1,9 @@
 package com.ll.medium.global.init;
 
+import com.ll.medium.domain.aticle.article.DTO.ArticleDto;
+import com.ll.medium.domain.aticle.article.entity.Article;
 import com.ll.medium.domain.aticle.article.service.ArticleService;
+import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.memberDto.MemberDto;
 import com.ll.medium.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -33,19 +36,18 @@ public class InitDevData {
                     .memberEmail("admin@user.com")
                     .build();
             memberService.joinMember(admin);
-//        Entity 문제인가?
-//            for (int i = 1; i < 4; i++) {
-//                Member member = Member.dtoToEntity(memberService.findById((long) i));
-//                for (int j = 0; j < i * 50; j++) {
-//                    Article article = Article.builder()
-//                            .title("제목" + j)
-//                            .body("내용" + j)
-//                            .published(true)
-//                            .build();
-//                    ArticleDto dto = new ArticleDto(article);
-//                    articleService.writeArticle(dto, member);
-//                }
-//            }
+            for (int i = 1; i < 4; i++) {
+                Member member = Member.dtoToEntity(memberService.findById((long) i));
+                for (int j = 0; j < i * 50; j++) {
+                    Article article = Article.builder()
+                            .title("제목" + j)
+                            .body("내용" + j)
+                            .published(true)
+                            .build();
+                    ArticleDto dto = new ArticleDto(article);
+                    articleService.writeArticle(dto, member);
+                }
+            }
         };
     }
 }
